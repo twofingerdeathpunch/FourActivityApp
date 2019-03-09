7fb43fe274a9945066b8082c083bb4c6697ad3dc
Miguel Najera <migueln1987@gmail.com>
2019-03-09 12:59:56 -0500

    Change version of Butterknife to work with project, Log in button goes to Profile activity

--- app/src/main/java/com/example/fouractivityapp/MainActivity.java
+++ app/src/main/java/com/example/fouractivityapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.fouractivityapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.login_button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.login_button)
+    public void logInButtonPress() {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/fouractivityapp/Profile.java
+++ app/src/main/java/com/example/fouractivityapp/Profile.java
@@ -2,6 +2,7 @@ package com.example.fouractivityapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Profile extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Log.d("Profile - onCreate", "In the second activity");
     }
 }
